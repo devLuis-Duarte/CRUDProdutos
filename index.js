9a0f2a572be038b1125239d3fa7e023b8f3f9196
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2024-03-13 15:38:22 -0300

    Adicionando cadastro do usuario incluindo hash na senha

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const connection  = require("./database/database");
 const User = require("./users/User");
 const UserController = require("./users/UserController");
+const BodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+
+app.use(BodyParser.urlencoded({extended: true}));
 
 connection.authenticate().then(() => {
     console.log("servidor conectado com sucesso");
--- users/UserController.js
+++ users/UserController.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 
 router.get("/user/create", (req, res) => {
     res.render("users/create");
 });
+
+router.post("/user/save", (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    
+    if(email != undefined){
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.compareSync(password, salt);
+
+        User.create({
+            name: name,
+            email: email,
+            password: hash,
+
+        }).then((user) => {
+            res.render("users/create", {
+                user: user,
+            })
+        })
+    }else {
+        res.redirect("/");
+    }
+})
 
 module.exports = router;
 
